9f3f98f02dfa878a320fdc340d81e3954c380032
Nikolai <nikolay.golovachev96@mail.ru>
2022-11-25 17:48:27 +0300

    Add Task 27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая 
+// 1. принимает на вход число и 
+// 2. выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int SumDigit(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+int sumDigit = SumDigit(number);
+Console.WriteLine($"Сумма цифр в числе: {sumDigit}");
