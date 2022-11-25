4456dfb4999986ca436967a2138d63d346acb428
Nikolai <nikolay.golovachev96@mail.ru>
2022-11-25 17:11:20 +0300

    corrected Task 25

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -5,7 +5,6 @@
 // 3, 5 -> 243
 // 2, 4 -> 16
 
-
 Console.WriteLine("Введите число A: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите число B: ");
@@ -13,15 +12,12 @@ int numberB = Convert.ToInt32(Console.ReadLine());
 
 int ExpNum(int numA, int numB)
 {
+    int exp = 1;
+    for (int i = 1; i <= numB; i++)
     {
-        int exp = 1;
-        for (int i = 1; i <= numB; i++)
-        {
-            exp = exp * numA;
-
-        }
-        return exp;
+        exp = exp * numA;
     }
+    return exp;
 }
 
 int expNum = ExpNum(numberA, numberB);
