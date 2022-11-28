26753c1c9d5da626aeba2bd7d2d6516edba13336
Nikolai <nikolay.golovachev96@mail.ru>
2022-11-28 20:51:10 +0300

    Added completed the task29

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -4,21 +4,25 @@
 
 // 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
 
-// int[] array = new int[8];
-// void FillArray(int[] arr)
-// {
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         arr[i] = new Random().Next(0, 2);
-//     }
-// }
-// void PrintArray(int[] arr)
-// {
-//     Console.Write("[");
-//     for (int i = 0; i < arr.Length; i++)
-//     {
-//         Console.Write($"{arr[i]} ");
-//     }
-//     Console.Write("]");
-// }
-// FillArray(array);
+int[] array = new int[8];
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 100);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+FillArray(array);
+PrintArray(array);
